239baad71ecd368fefe05c7ce9fc37bd94699a36
masella <gio.masella17@gmail.com>
2024-09-24 22:16:51 -0400

    Order Service for micro service project

---
+++ src/main/java/com/masella/microservices/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.masella.microservices.order.repository;
+
+import com.masella.microservices.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
