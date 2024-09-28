bfab68fdf9a45ebdbfedbe050fdf00e9df90fdcb
masella <gio.masella17@gmail.com>
2024-09-28 10:09:49 -0400

    added swagger and removed open feign

---
+++ src/main/java/com/masella/microservices/order/config/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package com.masella.microservices.order.config;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI orderServiceAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("Rest API for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("")
+                        .url("http://order-service-dummy-url/docs"));
+    }
+}
